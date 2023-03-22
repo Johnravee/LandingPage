32188aa62d077c001d6c4512ab8d5587fc2c428e
Johnrave <johnravemimayn@gmail.com>
2023-03-22 22:16:33 +0800

    DONE

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const cardslide = document.querySelector(".wrapper")
 
+ // Menus
+ const cclose = document.querySelector("#close")
+ const hamburger = document.querySelector(".hamburger");
+ const links = document.querySelector(".links")
+ const linkItem = document.querySelectorAll(".link-item")
 
 cardslide.addEventListener("mouseover", ()=>{
     cardslide.style.overflowX = "auto"
@@ -10,21 +15,28 @@ cardslide.addEventListener("mouseleave", ()=>{
 })
 
 
- // Menus
- const cclose = document.querySelector("#close")
- const hamburger = document.querySelector(".hamburger");
- const links = document.querySelector(".links")
+
 
  // add class 'left'
  hamburger.addEventListener("click", ()=>{
    links.classList.toggle("left")
+   document.body.classList.toggle("antiScroll")
  })
 
  //remove class 'left'
  cclose.addEventListener("click", ()=>{
    links.classList.remove("left")
+   document.body.classList.remove("antiScroll")
  })
+ 
 
+linkItem.forEach(item =>{
+
+  item.addEventListener('click', ()=>{
+    links.classList.remove("left")
+    document.body.classList.remove("antiScroll")
+  })
+})
 
 
 
